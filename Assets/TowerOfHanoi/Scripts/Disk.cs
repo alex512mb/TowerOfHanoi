9ac165f95ca4e569131e3fa2e8f20bad87b579f1
alex512mb <51264794+alex512mb@users.noreply.github.com>
2022-09-26 07:38:53 +0300

    All the files

---
+++ Assets/TowerOfHanoi/Scripts/Disk.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace TowerHanoi
+{
+    /// <summary>
+    /// The disk can move to a set destination
+    /// </summary>
+    public class Disk : MonoBehaviour
+    {
+        [SerializeField] float speedMove = 1;
+
+        Action OnComplete;
+        Vector3 destination;
+
+        float minDistanceToDestination
+        {
+            get { return speedMove * Time.deltaTime * 2; }
+        }
+
+        /// <summary>
+        /// Translate the disk to the destination
+        /// </summary>
+        /// <param name="_destination">the final destination of the disk</param>
+        /// <param name="onComplete">will be invoke after finish the work</param>
+        /// <param name="height">height where disk will move</param>
+        public void StartProgrammReplace(Vector3 _destination, float height, Action onComplete)
+        {
+            OnComplete = onComplete;
+            destination = _destination;
+            StartCoroutine(ProgramMoveToDestination(height));
+        }
+        /// <summary>
+        /// Cancel current movement if it is act
+        /// </summary>
+        public void Cancel()
+        {
+            StopAllCoroutines();
+        }
+
+        //Other methods
+        IEnumerator ProgramMoveToDestination(float height)
+        {
+            //wait for go up
+            yield return StartCoroutine(ProgramMoveToPos(new Vector3(transform.position.x, height, transform.position.z)));
+
+            //wait for go to over destination pos
+            yield return StartCoroutine(ProgramMoveToPos(new Vector3(destination.x, height, destination.z)));
+
+            //wait for go to destination
+            yield return StartCoroutine(ProgramMoveToPos(destination));
+
+            OnComplete();
+        }
+        IEnumerator ProgramMoveToPos(Vector3 pos)
+        {
+            float distanceToDestination = Mathf.Infinity;
+            while (distanceToDestination > minDistanceToDestination)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos, speedMove * Time.deltaTime);
+                distanceToDestination = Vector3.Distance(transform.position, pos);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
---
+++ Assets/TowerOfHanoi/Scripts/StockDisks.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace TowerHanoi
+{
+    /// <summary>
+    /// Storage disks in slots
+    /// </summary>
+    public class StockDisks : MonoBehaviour
+    {
+        //setting
+        [SerializeField] int Capacity = 16;
+        [SerializeField] float heightOneSlot = 0.1f;
+
+        //private data
+        Vector3[] slots;
+        List<Disk> disks = new List<Disk>();
+
+        //events
+        /// <summary>
+        /// Called when the mouse enters the stock
+        /// </summary>
+        public event Action<StockDisks> On_MouseEnter;
+        /// <summary>
+        /// Called when the mouse exits the stock
+        /// </summary>
+        public event Action<StockDisks> On_MouseExit;
+
+        #region MonoBehaviour events
+        void Awake()
+        {
+            slots = new Vector3[Capacity];
+            for (int i = 0; i < Capacity; i++)
+                slots[i] = transform.position + new Vector3(0, (i + 1) * heightOneSlot, 0);
+        }
+        void OnMouseEnter()
+        {
+            if (On_MouseEnter != null)
+                On_MouseEnter(this);
+        }
+        void OnMouseExit()
+        {
+            if (On_MouseExit != null)
+                On_MouseExit(this);
+        }
+        #endregion
+
+        #region public commands
+        /// <summary>
+        /// Add disk to the end of the stock
+        /// </summary>
+        /// <param name="disk"></param>
+        public void PutDiskOnTop(Disk disk)
+        {
+            disk.transform.position = slots[disks.Count];
+            disks.Add(disk);
+        }
+        /// <summary>
+        /// Remove last disk from the stock
+        /// </summary>
+        /// <returns></returns>
+        public Disk TakeLastDisk()
+        {
+            Disk lastDisk = disks[disks.Count - 1];
+            disks.RemoveAt(disks.Count - 1);
+            return lastDisk;
+        }
+        /// <summary>
+        /// Destroy all disks if any exists
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < disks.Count; i++)
+            {
+                Destroy(disks[i].gameObject);
+            }
+            disks.Clear();
+        }
+        /// <summary>
+        /// Return last free position in the stock
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetLastFreePos()
+        {
+            return slots[disks.Count];
+        }
+        #endregion
+
+    }
+}
